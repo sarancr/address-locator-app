1f2938161f6725361b39567e515276e8f7c79d57
Saranya CR <srny.cr@gmail.com>
2017-09-02 14:13:17 -0700

    initializing repo

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+var csv = require("fast-csv");
+const app = express();
+
+//Set static path
+app.use(express.static(path.join(__dirname, "public")));
+
+var physicianData = [];
+
+csv.fromPath("data/OP_DTL_OWNRSHP_PGYR2016_P06302017.csv")
+  .on("data", function(data) {
+    // console.log(data);
+    physicianData.push(data);
+  })
+  .on("end", function() {
+    console.log("done");
+    console.log(physicianData);
+  });
+
+function compare(str1, str2){
+    return str1 != null && str2!= null && str1.toLocaleLowerCase() === str2.toLocaleLowerCase();
+}
+
+app.get("/address", (req, res) => {
+  const fname = req.query.fname;
+  const mname = req.query.mname;
+  const lname = req.query.lname;
+  console.log(fname + " " + mname + "  " + lname);
+
+  var result = null;
+  for (var i = 0; i < physicianData.length; i++) {
+    var row = physicianData[i];
+    if (compare(row[2], fname) && compare(row[3], mname) && compare(row[4], lname)) {
+      result = row;
+      break;
+    }
+  }
+  //const result  = physicianData.find(record[2] === fname && record[3] === mname && record[4] === lname);
+  console.log("Physician match found  = " + result);
+  if (result != null){
+    res.send(result[6] + ", " + result[8] + ", " + result[9] + ", " + result[10] );
+  }else {
+      console.log("Error: No match found");
+      res.sendStatus(204);
+  }
+
+});
+
+app.listen(3000, () => {
+  console.log("Server listens at port 3000");
+});
