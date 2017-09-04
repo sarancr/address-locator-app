e6312e270b4e3901bbbcedc3a27f9efea67c7a9a
Saranya CR <srny.cr@gmail.com>
2017-09-04 13:40:05 -0700

    port update

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.get("/address", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log("Server listens at port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started..");
 });
